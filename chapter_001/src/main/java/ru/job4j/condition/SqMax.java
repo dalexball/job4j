b8ea87ad0c576d89fbcfd9a38c6088d73a9d7c15
Alex Dotsyak <docyak00@gmail.com>
2020-01-18 19:34:27 +0200

    roflanCurses MultiMax task

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = first;
+                }
+            }
+        } else if (second > third) {
+            if (second > forth) {
+                result = second;
+            }
+        } else if (third > forth) {
+            result = third;
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        int result = max(4,3,2,1);
+        System.out.println(result);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+
+    @Test
+    public void firstMax(){
+        int result=SqMax.max(5,4,3,2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void secondMax(){
+        int result=SqMax.max(7,9,4,5);
+        assertThat(result,is(9));
+    }
+
+    @Test
+    public void thirdMax(){
+        int result=SqMax.max(6,10,15,8);
+        assertThat(result, is(15));
+    }
+
+    @Test
+    public  void forthMax(){
+        int result=SqMax.max(6,12,20,25);
+        assertThat(result, is(25));
+    }
+
+    @Test
+    public void allEqual(){
+        int result=SqMax.max(7,7,7,7);
+        assertThat(result, is(7));
+    }
+}
