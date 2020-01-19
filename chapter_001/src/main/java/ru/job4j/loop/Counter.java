d5e36ce55cd65c7cf90c03c3b962293583bfa22f
Alex Dotsyak <docyak00@gmail.com>
2020-01-19 11:25:51 +0200

    roflanCurses Counter task

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Counter {
+
+    public static int add(int start, int finish){
+        int sum=0;
+        for (int curVal=start; curVal<=finish; curVal++){
+            if (curVal%2==0){
+                sum+=curVal;
+            }else continue;
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void from1To10(){
+        int result=Counter.add(1,10);
+        assertThat(result,is(30));
+    }
+
+}
