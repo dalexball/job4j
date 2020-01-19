907c5e3a58020850a2b16cae66006dba972a42db
Alex Dotsyak <docyak00@gmail.com>
2020-01-19 14:41:19 +0200

    roflanCurses JavaDoc task

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,61 @@
 package ru.job4j.calculator;
 
+/**
+ * This class contains operations between two variables of type double.
+ *
+ * @author Alex Dotsyak
+ * @version 1
+ */
 public class Calculator {
+    /**
+     * Prints result of adding of two vars.
+     *
+     * @param firstVal-firstVal
+     * @param secondVal-secondVal
+     */
     public static void add(double firstVal, double secondVal) {
         double result = firstVal + secondVal;
         System.out.println(firstVal + "+" + secondVal + "=" + result);
     }
 
+    /**
+     * Prints result of subtracting between two vars.
+     *
+     * @param firstVal-firstVal
+     * @param secondVal-secondVal
+     */
     public static void subtract(double firstVal, double secondVal) {
         double result = firstVal - secondVal;
         System.out.println(firstVal + "-" + secondVal + "=" + result);
     }
 
+    /**
+     * Prints result of multiplying of two vars.
+     *
+     * @param firstVal-firstVal
+     * @param secondVal-secondVal
+     */
     public static void multiply(double firstVal, double secondVal) {
         double result = firstVal * secondVal;
         System.out.println(firstVal + "*" + secondVal + "=" + result);
     }
 
+    /**
+     * Prints result of dividing between two vars.
+     *
+     * @param firstVal-firstVal
+     * @param secondVal-secondVal
+     */
     public static void div(double firstVal, double secondVal) {
         double result = firstVal / secondVal;
         System.out.println(firstVal + "/" + secondVal + "=" + result);
     }
 
+    /**
+     * Calculates some vars.
+     *
+     * @param args-args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculating tasks.
+ *
+ * @author Alex Dotsyak
+ * @version 1
+ */
+package ru.job4j.calculator;
