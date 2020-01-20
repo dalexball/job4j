f75609385817c3608867a8b101153bc7b49c6935
Alex Dotsyak <docyak00@gmail.com>
2020-01-20 13:19:25 +0200

    roflanCurses appearance-fix array.Square

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -8,13 +8,14 @@ package ru.job4j.array;
 public class Square {
     /**
      * Calculates squares of numbers
+     *
      * @param bound - bound
      * @return array of squares from 1 to bound
      */
-    public int[] calculate(int bound){
-        int[] rst=new int[bound];
-        for (int index=0;index!=bound;index++){
-            rst[index]=(index+1)*(index+1);
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int index = 0; index != bound; index++) {
+            rst[index] = (index + 1) * (index + 1);
         }
         return rst;
     }
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,35 +11,35 @@ public class SquareTest {
      * Check if method calculate works with number 3
      */
     @Test
-    public void whenBound3Then149(){
-        int bound=3;
-        Square ar3=new Square();
-        int[] result=ar3.calculate(bound);
-        int[] expect=new int[]{1,4,9};
-        assertThat(result,is(expect));
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square ar3 = new Square();
+        int[] result = ar3.calculate(bound);
+        int[] expect = new int[]{1, 4, 9};
+        assertThat(result, is(expect));
     }
 
     /**
      * Check if method calculate works with number 4
      */
     @Test
-    public void whenBound4Then14916(){
-        int bound=4;
-        Square ar3=new Square();
-        int[] result=ar3.calculate(bound);
-        int[] expect=new int[]{1,4,9,16};
-        assertThat(result,is(expect));
+    public void whenBound4Then14916() {
+        int bound = 4;
+        Square ar3 = new Square();
+        int[] result = ar3.calculate(bound);
+        int[] expect = new int[]{1, 4, 9, 16};
+        assertThat(result, is(expect));
     }
 
     /**
      * Check if method calculate returns empty array if bound equals zero;
      */
     @Test
-    public void whenBound0ThenNull(){
-        int bound=0;
-        Square ar3=new Square();
-        int[] result=ar3.calculate(bound);
-        int[] expect=new int[]{};
-        assertThat(result,is(expect));
+    public void whenBound0ThenNull() {
+        int bound = 0;
+        Square ar3 = new Square();
+        int[] result = ar3.calculate(bound);
+        int[] expect = new int[]{};
+        assertThat(result, is(expect));
     }
 }
