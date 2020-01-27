e07fe37a320b005caf5585c8165a4102616739af
Alex Dotsyak <docyak00@gmail.com>
2020-01-27 21:25:03 +0200

    Соединить два отсортированных массива fix

--- chapter_001/src/main/java/ru/job4j/exam/AddArrays.java
+++ chapter_001/src/main/java/ru/job4j/exam/AddArrays.java
@@ -10,51 +10,36 @@ public class AddArrays {
 
     /**
      * Method combines sorted arrays while also self-sorting
-     *
-     * @param ar1 - first array
+     * * @param ar1 - first array
      * @param ar2 - second array
      * @return combined array
      */
     public int[] addArs(int[] ar1, int[] ar2) {
-        int[] resArray = new int[(ar1.length + ar2.length)];
-        int firstArIndex = 0, secondArIndex = 0;
-        int i = 0;
-        while (i < resArray.length) {
-            if (secondArIndex < ar2.length) {
-                while (firstArIndex < ar1.length && ar1[firstArIndex] < ar2[secondArIndex]) {
-                    resArray[i] = ar1[firstArIndex];
-                    i++;
-                    firstArIndex++;
+        int[] result = new int[(ar1.length + ar2.length)];
+        int i1 = 0, i2 = 0;
+        int resI = 0;
+        for (; resI < result.length; resI++) {
+            if (i1 < ar1.length && i2 < ar2.length) {
+                if (ar1[i1] < ar2[i2]) {
+                    result[resI] = ar1[i1];
+                    i1++;
+                } else {
+                    result[resI] = ar2[i2];
+                    i2++;
                 }
             } else {
-                while (firstArIndex < ar1.length) {
-                    resArray[i] = ar1[firstArIndex];
-                    i++;
-                    firstArIndex++;
-                }
-            }
-            if (firstArIndex < ar1.length) {
-                while (secondArIndex < ar2.length && ar2[secondArIndex] < ar1[firstArIndex]) {
-                    resArray[i] = ar2[secondArIndex];
-                    i++;
-                    secondArIndex++;
-                }
-            } else {
-                while (secondArIndex < ar2.length) {
-                    resArray[i] = ar2[secondArIndex];
-                    i++;
-                    secondArIndex++;
-                }
-            }
-            if (firstArIndex < ar1.length && secondArIndex < ar2.length && ar1[firstArIndex] == ar2[secondArIndex]) {
-                resArray[i] = ar1[firstArIndex];
-                i++;
-                firstArIndex++;
-                resArray[i] = ar2[secondArIndex];
-                i++;
-                secondArIndex++;
+                break;
             }
         }
-        return resArray;
+        for (; resI < result.length; resI++) {
+            if (i1 == ar1.length) {
+                result[resI] = ar2[i2];
+                i2++;
+            } else if (i2 == ar2.length) {
+                result[resI] = ar1[i1];
+                i1++;
+            }
+        }
+        return result;
     }
 }
