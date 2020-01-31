4c439ffe8f214a42617d6e8ed0b018e0322a8f5f
Alex Dotsyak <docyak00@gmail.com>
2020-01-31 18:01:37 +0200

    Triangle upgrade

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,18 +1,53 @@
 package ru.job4j.condition;
 
+/**
+ * Class models triangle
+ *
+ * @author Alex Dotsyak
+ * @version 2
+ */
 public class Triangle {
-    public static boolean exists(double ab, double ac, double bc) {
-        double sum = ab + ac;
-        if (sum > bc) {
-            sum = ab + bc;
-            if (sum > ac) {
-                sum = ac + bc;
-                return sum > ab;
-            } else {
-                return false;
+
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point first, Point second, Point third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    /**
+     * Checks if triangle exists
+     * ab,bc,ac - triangle sides
+     *
+     * @return true, if triangle exists, false if doesn't
+     */
+    public boolean exists() {
+        boolean result = false;
+        double ab = Point.distance(first, second);
+        double ac = Point.distance(first, third);
+        double bc = Point.distance(second, third);
+        if ((ab + ac) > bc) {
+            if ((ab + bc) > ac) {
+                if ((ac + bc) > ab) {
+                    result = true;
+                }
             }
-        } else {
-            return false;
         }
+        return result;
+    }
+
+    public static boolean exists(double first, double second, double third){
+        boolean result=false;
+        if ((first + second) > third) {
+            if ((first + third) > second) {
+                if ((second + third) > first) {
+                    result = true;
+                }
+            }
+        }
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,20 +1,46 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test class for Triangle.class
+ *
+ * @author Alex Dotsyak
+ * @version 2
+ */
 public class TriangleTest {
 
+    /**
+     * Checks if static method "exists" works fine if result is true
+     */
     @Test
     public void whenExists() {
         boolean result = Triangle.exists(2, 2, 2);
         assertThat(result, is(true));
     }
 
+    /**
+     * Checks if static method "exists" works fine if result is false
+     */
     @Test
     public void whenDoesNotExist() {
         boolean result = Triangle.exists(1, 1, 5);
         assertThat(result, is(false));
+    }
+
+    /**
+     * Checks if non-static method "exists" works fine
+     */
+    @Test
+    public void whenExistsObject() {
+        Point first = new Point(0, 0);
+        Point second = new Point(3, 0);
+        Point third = new Point(0, 3);
+        Triangle triangle = new Triangle(first, second, third);
+        boolean result = triangle.exists();
+        assertThat(result, is(true));
     }
 }
