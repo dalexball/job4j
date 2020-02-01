98ada559b1969db749b3e0a31620176319c0c99b
Alex Dotsyak <docyak00@gmail.com>
2020-02-01 21:07:56 +0200

    Перегрузить метод max для 3, 4 чисел

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,10 +1,43 @@
 package ru.job4j.condition;
 
+/**
+ * Utility class for finding highest number
+ *
+ * @author Alex Dotsyak
+ * @version 2
+ */
 public class Max {
+    /**
+     * Find highest between two
+     *
+     * @param firstVal  - firstVal
+     * @param secondVal - secondVal
+     * @return highest
+     */
     public static int max(int firstVal, int secondVal) {
         int result;
         boolean higher = firstVal >= secondVal;
         result = higher ? firstVal : secondVal;
+        return result;
+    }
+
+    /**
+     * Find highest between three
+     *
+     * @param firstVal  - firstVal
+     * @param secondVal - secondVal
+     * @param thirdVal  -thirdVal
+     * @return highest
+     */
+    public static int max(int firstVal, int secondVal, int thirdVal) {
+        int result;
+        result = max(firstVal, max(secondVal, thirdVal));
+        return result;
+    }
+
+    public static int max(int firstVal, int secondVal, int thirdVal, int fourthVal) {
+        int result;
+        result = max(max(firstVal, secondVal, thirdVal), fourthVal);
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-
+/**
+ * Test class for Max.class
+ *
+ * @author Alex Dotsyak
+ * @version 2
+ */
 public class MaxTest {
 
     @Test
@@ -23,6 +28,18 @@ public class MaxTest {
     @Test
     public void bothEqual() {
         int result = Max.max(4, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void thirdHighest() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void fourthHighest(){
+        int result = Max.max(1,2,3,4);
         assertThat(result, is(4));
     }
 }
