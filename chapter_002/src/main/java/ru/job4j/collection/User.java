4155e7bc230f94bb9cc8d17b7a4a9410ec3e50a0
Alex Dotsyak <docyak00@gmail.com>
2020-02-27 23:01:18 +0200

    Организовать ортировку User

---
+++ chapter_002/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,55 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+/**
+ * Class models user
+ *
+ * @author Alex Dotsyak
+ * @version 1
+ * @since 0.1
+ */
+public class User implements Comparable<User> {
+
+    /**
+     * Name and age of user
+     */
+    private String name;
+    private int age;
+
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * Compare this user to input user
+     *
+     * @param o - input user
+     * @return 0 if this = o, >0 if this > o, <0 if this < o
+     */
+    @Override
+    public int compareTo(User o) {
+        int value = this.name.compareTo(o.name);
+        if (value == 0) {
+            value = age - o.age;
+        }
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+}
---
+++ chapter_002/src/test/java/ru/job4j/collection/UserTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for User class
+ *
+ * @author Alex Dotsyak
+ * @version 1
+ * @since 0.1
+ */
+public class UserTest {
+
+    /**
+     * Checks if items are sorted after being added to TreeSet
+     */
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        assertThat(it.next(), is(new User("Ivan", 31)));
+        assertThat(it.next(), is(new User("Petr", 32)));
+    }
+
+    /**
+     * Checks if value of user Petr is greater than value of user Ivan
+     */
+    @Test
+    public void whenComparePertVSIvan() {
+        int rsl = new User("Petr", 32)
+                .compareTo(
+                        new User("Ivan", 31)
+                );
+        assertThat(rsl, greaterThan(0));
+    }
+}
