cf40e9ede3dc7682eea1cd67da8055b362b69fb2
Alex Dotsyak <docyak00@gmail.com>
2020-03-03 14:10:43 +0200

    User getters

--- chapter_002/src/main/java/ru/job4j/collection/User.java
+++ chapter_002/src/main/java/ru/job4j/collection/User.java
@@ -23,6 +23,14 @@ public class User implements Comparable<User> {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     /**
      * Compare this user to input user
      *
